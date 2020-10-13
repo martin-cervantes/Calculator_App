16122796a7db32f43b3034036896a3262fc58a63
Martin Cervantes <cervantes.martine@gmail.com>
2020-10-13 12:32:55 -0600

    Implement the ButtonPanel component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Display from './Display';
-import Button from './Button';
+import ButtonPanel from './ButtonPanel';
 
 export default function App() {
   return (
     <div className="calculator">
       <Display />
-      <Button />
+      <ButtonPanel />
     </div>
   );
 }
