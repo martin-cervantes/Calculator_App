7e459b6f64f6a03aa652282a9b7cfc9d63503176
Martin Cervantes <cervantes.martine@gmail.com>
2020-10-14 18:19:11 -0600

    Add calculate logic for buttons

---
+++ src/logic/calculate.js
@@ -0,0 +1,48 @@
+import operate from './operate';
+
+export default function calculate(data, buttonName) {
+  let { total, next, operation } = data;
+  const operator = ['+', '-', 'x', '÷', '%'];
+  const digits = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+  if (buttonName === 'AC') {
+    total = null;
+    next = null;
+    operation = null;
+  } else if (digits.includes(buttonName)) {
+    if (operation === '=') {
+      total = null;
+      next = null;
+      operation = null;
+    }
+
+    if (total === null && next === null && operation === null) {
+      total = buttonName;
+    } else if (next === null && operation === null) {
+      if (buttonName === '.') {
+        total = total.includes(buttonName) ? `${total}` : total + buttonName;
+      } else {
+        total += buttonName;
+      }
+    } else if (next === null) {
+      next = buttonName;
+    } else if (buttonName === '.') {
+      next = next.includes(buttonName) ? `${next}` : next + buttonName;
+    } else {
+      next += buttonName;
+    }
+  } else if (operator.includes(buttonName)) {
+    if (total !== null) operation = buttonName;
+  } else if (buttonName === '+/-' && next !== null) {
+    if (total !== null) total *= -1;
+    if (next !== null) next *= -1;
+  } else if (buttonName === '=') {
+    if (total !== null && next !== null && operation !== null) {
+      total = operate(total, next, operation);
+      next = null;
+      operation = '=';
+    }
+  }
+
+  return { total, next, operation };
+}
