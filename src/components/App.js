370da1e66964581d8204fc36093c963bb906e5e0
Martin Cervantes <cervantes.martine@gmail.com>
2020-10-14 19:43:45 -0600

    Add constructor and render method to App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-export default function App() {
-  return (
-    <div className="calculator">
-      <Display />
-      <ButtonPanel />
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
 }
