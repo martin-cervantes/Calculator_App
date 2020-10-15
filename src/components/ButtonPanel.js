44f60726e67608028094f70da89c69066de9e7ca
Martin Cervantes <cervantes.martine@gmail.com>
2020-10-14 19:57:25 -0600

    Implement the handleClick function which takes buttonName as an argument and returns clickHandler from props.

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,42 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Button from './Button';
 
-export default function ButtonPanel() {
+export default function ButtonPanel(props) {
+  const { handleClick } = props;
+
   return (
     <div className="buttonPanel">
       <div className="group">
-        <Button name="AC" color="gray" />
-        <Button name="+/-" color="gray" />
-        <Button name="%" color="gray" />
-        <Button name="÷" />
+        <Button name="AC" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="+/-" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="%" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="÷" clickHandler={buttonName => handleClick(buttonName)} />
       </div>
 
       <div className="group">
-        <Button name="7" color="gray" />
-        <Button name="8" color="gray" />
-        <Button name="9" color="gray" />
-        <Button name="x" />
+        <Button name="7" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="8" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="9" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="x" clickHandler={buttonName => handleClick(buttonName)} />
       </div>
 
       <div className="group">
-        <Button name="4" color="gray" />
-        <Button name="5" color="gray" />
-        <Button name="6" color="gray" />
-        <Button name="-" />
+        <Button name="4" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="5" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="6" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="-" clickHandler={buttonName => handleClick(buttonName)} />
       </div>
 
       <div className="group">
-        <Button name="1" color="gray" />
-        <Button name="2" color="gray" />
-        <Button name="3" color="gray" />
-        <Button name="+" />
+        <Button name="1" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="2" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="3" color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="+" clickHandler={buttonName => handleClick(buttonName)} />
       </div>
 
       <div className="group">
-        <Button name="0" color="gray" wide />
-        <Button name="." color="gray" />
-        <Button name="=" />
+        <Button name="0" color="gray" wide clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="." color="gray" clickHandler={buttonName => handleClick(buttonName)} />
+        <Button name="=" clickHandler={buttonName => handleClick(buttonName)} />
       </div>
     </div>
   );
 }
+
+ButtonPanel.propTypes = {
+  handleClick: PropTypes.func,
+};
+
+ButtonPanel.defaultProps = {
+  handleClick: '',
+};
